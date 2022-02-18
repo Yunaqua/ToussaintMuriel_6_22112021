6044ae0c48793c3b94e0f59ce16d115556550135
Muriel <toussaintmuriel@yahoo.com>
2022-02-18 18:14:26 +0100

    test sauce

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -5,7 +5,7 @@ require('dotenv').config({ path: './.env' });
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(' ')[1]; //la partie du tableau qui contient l'authentification
-    const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET'); //la clé doit correspondre à la fonction login
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET); //la clé doit correspondre à la fonction login
     const userId = decodedToken.userId;
     //req.auth = { userId };  //{ userId : userId};  raccourci js quand la clé et la variable ont le meme nom
     if (req.body.userId && req.body.userId !== userId) { //si le corps de la requete a un user.id, verifie que l'id correspond avec celui du token
