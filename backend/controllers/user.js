da9c59d6d9041d6b72751bfa1fc8f2c6c2d3c2bd
Muriel <toussaintmuriel@yahoo.com>
2022-03-27 03:15:04 +0200

    protection requete postman création d'utilisateur

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -2,21 +2,30 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 //require('dotenv').config({ path: './.env' });
+const reMail = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
+const str = 'yyooo@live.te';
+const regex = new RegExp(reMail);
+//console.log(regex.test(str));
 
-exports.signup = (req, res, next) => {
-  bcrypt.hash(req.body.password, 10)
-    .then(hash => {
-      const user = new User({
-        email: req.body.email,
-        password: hash
-      });
-      user.save()
-        .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
-        .catch(error => res.status(400).json({ error }));
-    })
-    .catch(error => res.status(500).json({ error }));
-};
+  exports.signup = (req, res, next) => {
+    bcrypt.hash(req.body.password, 10)
+      .then(hash => {
+          if((regex.test(req.body.email)) == true){
+            const user = new User({
+              email: req.body.email,
+              password: hash
+            });
+          user.save()
+            .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
+            .catch(error => res.status(400).json({ error }));
+          }else{
+            console.log("Ce n'est pas un email");
+          }
+          
+      })
+      .catch(error => res.status(500).json({ error }));
+  };
 
 exports.login = (req, res, next) => {
 
